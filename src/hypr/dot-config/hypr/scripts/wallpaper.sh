6d6ab86b71b8a1d47bd630958ce02034ca41d835
Nico Pareigis <nico@pareigis.de>
2025-09-18 15:28:39 +0200

    feat(hypr): allow setting wallpaper from script

--- src/hypr/dot-config/hypr/scripts/wallpaper.sh
+++ src/hypr/dot-config/hypr/scripts/wallpaper.sh
@@ -4,3 +4,9 @@ typeset -a files=(${XDG_PICTURE_HOME:-~/Pictures}/Wallpapers/ivan_shishkin/*)
 typeset -i i=$((RANDOM % ${#files[@]}))
 
 cp "${files[$i]}" ~/.wallpaper
+
+if [[ $1 == --set ]]; then
+  hyprctl hyprpaper unload ~/.wallpaper &>/dev/null \
+    && hyprctl hyprpaper preload ~/.wallpaper &>/dev/null \
+    && hyprctl hyprpaper wallpaper ,~/.wallpaper &>/dev/null
+fi
