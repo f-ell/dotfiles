551b152b5bd05bd1b29f639b2e9264f895b9f4da
Nico Pareigis <nico@pareigis.de>
2025-09-19 22:52:38 +0200

    feat(hyprlock): prominent partial blur using magick

--- src/hypr/dot-config/hypr/scripts/wallpaper.sh
+++ src/hypr/dot-config/hypr/scripts/wallpaper.sh
@@ -5,6 +5,16 @@ typeset -i i=$((RANDOM % ${#files[@]}))
 
 cp "${files[$i]}" ~/.wallpaper
 
+if command -v magick; then
+  magick ~/.wallpaper \
+    -gravity center -crop 16:9 -scale 1920x1080 \
+    -gravity northwest -region 640x1080+0+0 -blur 0x20 \
+    -fill black -colorize 10% \
+    +repage ~/.wallpaper-lock
+else
+  cp ~/.wallpaper{,-lock}
+fi
+
 if [[ $1 == --set ]]; then
   hyprctl hyprpaper unload ~/.wallpaper &>/dev/null \
     && hyprctl hyprpaper preload ~/.wallpaper &>/dev/null \
