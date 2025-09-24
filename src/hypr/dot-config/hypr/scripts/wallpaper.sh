b73240da9546c3e548d8bb440fb0a5ec42a8f848
Nico Pareigis <nico@pareigis.de>
2025-09-24 22:49:29 +0200

    feat(hypr): allow explicit file for setting wallpaper

--- src/hypr/dot-config/hypr/scripts/wallpaper.sh
+++ src/hypr/dot-config/hypr/scripts/wallpaper.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-typeset -a files=(${XDG_PICTURE_HOME:-~/Pictures}/Wallpapers/ivan_shishkin/*)
-typeset -i i=$((RANDOM % ${#files[@]}))
+typeset set=0
+if [[ $1 == --set ]]; then
+  set=1
+  shift
+fi
 
-cp "${files[$i]}" ~/.wallpaper
+if [[ $1 == -- ]]; then
+  shift
+fi
 
-if command -v magick; then
+typeset file=
+if [[ -f $1 ]]; then
+  file="$1"
+else
+  typeset -a files=(${XDG_PICTURE_HOME:-~/Pictures}/Wallpapers/ivan_shishkin/*)
+  typeset -i i=$((RANDOM % ${#files[@]}))
+
+  file="${files[$i]}" 
+fi
+
+cp "$file" ~/.wallpaper
+
+if command -v magick &>/dev/null; then
   magick ~/.wallpaper \
-    -gravity center -crop 16:9 -scale 1920x1080 \
+    -gravity center -crop 16:9 -scale 1920x1080\> \
     -gravity northwest -region 640x1080+0+0 -blur 0x20 \
     -fill black -colorize 10% \
     +repage ~/.wallpaper-lock
+
+  magick ~/.wallpaper \
+    -gravity center -crop 16:9 -scale 1920x1080\> \
+    -scale 40% \
+    +repage ~/.wallpaper-lowres
 else
-  cp ~/.wallpaper{,-lock}
+  cp ~/.wallpaper{,-lock,-scaled}
 fi
 
-if [[ $1 == --set ]]; then
+if (( $set == 1 )); then
   hyprctl hyprpaper unload ~/.wallpaper &>/dev/null \
     && hyprctl hyprpaper preload ~/.wallpaper &>/dev/null \
     && hyprctl hyprpaper wallpaper ,~/.wallpaper &>/dev/null
